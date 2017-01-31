5612cfef8c936de6929f738b25e1789e238b8352
Alain Metge <alain.metge@zenasoft.com>
2017-01-31 11:21:27 +0100

    add staging option

--- assets/cert-creation.sh
+++ assets/cert-creation.sh
@@ -4,11 +4,15 @@ set -e
 domain=$1
 email=$2
 
+staging=""
+if [ "$VULCAIN_ENV_MODE" == "test" ]; then
+    staging="--staging"
+fi
+
 folder="/etc/letsencrypt/live/$domain"
 
 if [ ! -d $folder ]; then
-    certbot certonly --text -n --keep --email $email --agree-tos --webroot -w /app/letsencrypt -d $domain
-    #    --staging
+    certbot certonly --text -n --keep --email $email --agree-tos --webroot -w /app/letsencrypt -d $domain $staging
     if [ -e ${folder}/fullchain.pem ]; then
         cat ${folder}/privkey.pem ${folder}/fullchain.pem | tee ${folder}/haproxy.pem >/dev/null
     fi
